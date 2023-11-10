a5fdb9b6f52b2ec71fa84aefb703cdb3c3972e5a
Yuvraj Singh Mor <140009504+yuvimor@users.noreply.github.com>
2023-11-10 20:39:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import streamlit as st
-import pickle
+import joblib
 import numpy as np
 
 # Load the trained model
-model_path = "model.pkl"
-with open(model_path, "rb") as file:
-    model = pickle.load(file)
+model_path = "model.joblib"
+model = joblib.load(model_path)
 
 # Streamlit App
 def main():
@@ -13,7 +12,10 @@ def main():
 
     # Sidebar with user inputs
     st.sidebar.header("User Input Features")
-    weekday_hours = st.sidebar.selectbox("Average Smartphone Use on Weekdays", ["<5 hours", ">5 hours"])
+    
+    # Update this line to use radio button for single choice selection
+    weekday_hours = st.sidebar.radio("Average Smartphone Use on Weekdays", ["<5 hours", ">5 hours"])
+    
     compulsive_behavior = st.sidebar.slider("Compulsive Behavior", 9, 36, 18)
     functional_impairment = st.sidebar.slider("Functional Impairment", 8, 32, 16)
     withdrawal = st.sidebar.slider("Withdrawal Symptoms", 6, 24, 12)
